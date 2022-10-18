e27076b7fee044358ed4159995c1e670d123250b
БыковаАА <@106-02>
2022-10-18 12:21:26 +0300

    Добавьте файлы проекта.

---
+++ Bykova41p_Pr6/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bykova41p_Pr6
+{
+    internal class Base
+    {
+        public static Entities ES;
+    }
+}
